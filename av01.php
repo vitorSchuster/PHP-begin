872d689f8bee1819e32914c210dd637a8bcbc045
vitor <vitorluisschuster@gmail.com>
2024-03-07 23:12:39 -0300

    agora sim
    apenas adicionei alguns aquivos

---
+++ av01.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        print"teste ";
+        print"olá mundo \n";
+        print"Escape 'chars' são os mesmos como C\n";
+        print"você pode ter quebra de linhas em uma string.";
+        print'Uma String pode usar "aspas duplas ou simples" e isso é muito legal. ';
+        print'Ainda pode-se usar aspas simples dessa forma it\'s cool ';
+    ?>
+</body>
+</html>
---
+++ av04.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo"texto <br>";
+        echo"olá mundo <br>";
+        echo"Isso abrange varias linhas as novas linhas serão saída também. <br>";
+        echo"Isso abrange\nmultiplas linhas. A nova linha será\na saída tambem. <br>";
+        echo"caracteres escaping são feitos \"como esse\". <br>";
+    ?>
+</body>
+</html>
